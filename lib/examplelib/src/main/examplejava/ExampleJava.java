6b5cdcbf6931ccf370bb399433827091e11f7eee
Erik Mayrhofer <obyoxar@gmail.com>
2017-12-07 17:09:33 +0100

    Added Gradle Project
    Added Gradle configuration

---
+++ lib/examplelib/src/main/examplejava/ExampleJava.java
@@ -0,0 +1,7 @@
+package examplejava;
+
+public class ExampleJava {
+    public static String getHelloWorld(){
+        return "Hello World!";
+    }
+}
---
+++ src/main/bobworga/main/App.java
@@ -0,0 +1,7 @@
+package bobworga.main;
+
+class App{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}

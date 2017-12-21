bb5e12ba1e8a4f4ba1c3d01d7435d08f19d399d3
Unknown <florian.schwarcz@gmx.at>
2017-12-21 18:06:14 +0100

    Added Staff and OrganisaionalStructureTests
    + Staff.java
    + PositionTests.java
    + StaffTests.java
    + Hierarchy Instance
    + Profile.compareAllHigherOrEqual
    * Hierarchy Test T01
    * getBestJob
    * Fixed typo "Subortinate"

--- lib/OrganisationalStructurelib/src/test/at/htl/florianschwarcz/organisationalstructurelibtests/HierarchyTests.java
+++ lib/OrganisationalStructurelib/src/test/at/htl/florianschwarcz/organisationalstructurelibtests/HierarchyTests.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class HierarchyTests {
     @Test
-    public void T01_SetHeadAndGetHead(){
+    public void T01_SetterAndGetter(){
         Hierarchy hierarchy = new Hierarchy();
         assertNull("Head should be null", hierarchy.getHead());
         Position head = new Position();
@@ -40,15 +40,15 @@ public class HierarchyTests {
         Position headSub = new Position();
         Position headSub2 = new Position();
         hierarchy.setHead(head);
-        head.addSubortinate(headSub);
-        head.addSubortinate(headSub2);
+        head.addSubordinate(headSub);
+        head.addSubordinate(headSub2);
         List<Position> expected = new LinkedList<>();
         expected.add(head);
         expected.add(headSub);
         expected.add(headSub2);
         assertEquals("List should be like expected", expected, hierarchy.positionList());
         Position headSubSub = new Position();
-        headSub.addSubortinate(headSubSub);
+        headSub.addSubordinate(headSubSub);
         expected.add(2, headSubSub);
         assertEquals("Expanded List should be like expected", expected, hierarchy.positionList());
     }
@@ -79,8 +79,8 @@ public class HierarchyTests {
         Person headSub2Person = new Person("Test3","Test", new Date(), "Test", "Test", 1, "Test", "Test", "Test", "Test", new Profile());
         headSub2.setPerson(headSub2Person);
         hierarchy.setHead(head);
-        head.addSubortinate(headSub);
-        head.addSubortinate(headSub2);
+        head.addSubordinate(headSub);
+        head.addSubordinate(headSub2);
         List<Person> expected = new LinkedList<>();
         expected.add(headPerson);
         expected.add(headSubPerson);
@@ -89,7 +89,7 @@ public class HierarchyTests {
         Position headSubSub = new Position();
         Person headSubSubPerson = new Person("Test4","Test", new Date(), "Test", "Test", 1, "Test", "Test", "Test", "Test", new Profile());
         headSubSub.setPerson(headSubSubPerson);
-        headSub.addSubortinate(headSubSub);
+        headSub.addSubordinate(headSubSub);
         expected.add(2, headSubSubPerson);
         assertEquals("Expanded List should be like expected", expected, hierarchy.personList());
     }
@@ -121,7 +121,7 @@ public class HierarchyTests {
         headSubPersonProfile.addAttribute("Datenbanken", 10);
         Person headSubPerson = new Person("Test1","Test", new Date(), "Test", "Test", 1, "Test", "Test", "Test", "Test", headSubPersonProfile);
         headSub.setPerson(headPerson);
-        head.addSubortinate(headSub);
+        head.addSubordinate(headSub);
         assertEquals("Head's sub should be best person", headSubPerson, hierarchy.getBestPerson(profile));
 
     }

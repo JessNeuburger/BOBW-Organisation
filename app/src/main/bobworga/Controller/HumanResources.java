61b73f66660ef83ee5b9a40c8c282d9519dd8057
JanNeuburger <jan.neuburger.jn@gmail.com>
2018-01-18 10:08:16 +0100

    Tabs are working now
    embedded Tab files into the main window

--- app/src/main/bobworga/Controller/HumanResources.java
+++ app/src/main/bobworga/Controller/HumanResources.java
@@ -1,4 +1,20 @@
 package bobworga.Controller;
 
-public class HumanResources {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class HumanResources extends BorderPane{
+    public HumanResources(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Tabs/HumanResources.fxml"));
+        loader.setController(this);
+        loader.setRoot(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            System.out.println("BLUP");
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/bobworga/Controller/OrganizationChart.java
+++ app/src/main/bobworga/Controller/OrganizationChart.java
@@ -1,5 +1,20 @@
 package bobworga.Controller;
 
-public class OrganizationChart {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+
+public class OrganizationChart extends AnchorPane{
+    public OrganizationChart(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Tabs/OrganizationChart.fxml"));
+        loader.setController(this);
+        loader.setRoot(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            System.out.println("BLUP");
+            e.printStackTrace();
+        }
+    }
 }
